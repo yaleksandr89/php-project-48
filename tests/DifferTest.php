f7b2146194e6128b7441d2002a4e29fa193f556b
Aleksandr Yurchenko <y.aleksandr89@yandex.ru>
2025-11-12 01:59:09 +0300

    Fix Stylish.php and fixtures

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -12,27 +12,27 @@ final class DifferTest extends TestCase
 {
     public function testGenDiffStylish(): void
     {
-        $expected = file_get_contents(__DIR__ . '/Fixtures/expected_stylish.txt');
-        $file1 = __DIR__ . '/Fixtures/nested_file1.json';
-        $file2 = __DIR__ . '/Fixtures/nested_file2.json';
+        $expected = file_get_contents(__DIR__ . '/Fixtures/stylishExcepted.txt');
+        $file1 = __DIR__ . '/Fixtures/file1.json';
+        $file2 = __DIR__ . '/Fixtures/file2.json';
 
         $this->assertSame($expected, genDiff($file1, $file2, 'stylish'));
     }
 
     public function testGenDiffPlain(): void
     {
-        $expected = file_get_contents(__DIR__ . '/Fixtures/expected_plain.txt');
-        $file1 = __DIR__ . '/Fixtures/nested_file1.yml';
-        $file2 = __DIR__ . '/Fixtures/nested_file2.yml';
+        $expected = file_get_contents(__DIR__ . '/Fixtures/plainExcepted.txt');
+        $file1 = __DIR__ . '/Fixtures/file1.yml';
+        $file2 = __DIR__ . '/Fixtures/file2.yml';
 
         $this->assertSame($expected, genDiff($file1, $file2, 'plain'));
     }
 
     public function testGenDiffJson(): void
     {
-        $expected = file_get_contents(__DIR__ . '/Fixtures/expected_json.txt');
-        $file1 = __DIR__ . '/Fixtures/nested_file1.json';
-        $file2 = __DIR__ . '/Fixtures/nested_file2.json';
+        $expected = file_get_contents(__DIR__ . '/Fixtures/jsonExcepted.txt');
+        $file1 = __DIR__ . '/Fixtures/file1.json';
+        $file2 = __DIR__ . '/Fixtures/file2.json';
 
         $this->assertSame($expected, genDiff($file1, $file2, 'json'));
     }
